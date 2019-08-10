2f95a8f386eb5024af487559933ee7dc2ef3a504
Benkassou Ali <ali.benkassou@insa-rouen.fr>
2019-08-10 03:16:46 +0100

    Add session number and stop watch to UI

--- src/Ui.java
+++ src/Ui.java
@@ -1,42 +1,87 @@
-import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.Duration;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
-public class Ui extends JPanel implements ActionListener {
 
-  boolean inProgress;
-  Session sess;
+public class Ui extends JPanel {
+
+  private boolean inProgress;
+  private Session sess;
+  private JLabel stopWatch;
+  private JLabel sessionNumber;
+  private long lastTickTime;
+  private Timer timer;
 
   public Ui() {
     inProgress = false;
-    sess = new Session();
+
+    sessionNumber = new JLabel("Session no.    ");
+
+    stopWatch = new JLabel(String.format("%02d:%02d:%02d", 0, 0, 0));
+    timer = new Timer(100, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            long runningTime = System.currentTimeMillis() - lastTickTime;
+            Duration duration = Duration.ofMillis(runningTime);
+            long hours = duration.toHours();
+            duration = duration.minusHours(hours);
+            long minutes = duration.toMinutes();
+            duration = duration.minusMinutes(minutes);
+            long millis = duration.toMillis();
+            long seconds = millis / 1000;
+            stopWatch.setText(
+              String.format("%04d:%02d:%02d", hours, minutes, seconds)
+            );
+        }
+    });
 
     JButton startButton = new JButton("Start");
     JButton stopButton = new JButton("Stop");
-    startButton.addActionListener(this);
-    stopButton.addActionListener(this);
+    startButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        if (!inProgress) {
+          sess = new Session();
+          sess.start();
+          sessionNumber.setText(String.format("Session no. %d", sess.getId()));
+          System.out.print("Session started... ");
+          lastTickTime = System.currentTimeMillis();
+          timer.start();
+          inProgress = true;
+        }
+      }
+    });
+    stopButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        if (inProgress) {
+          System.out.println("Session stopped");
+          sess.stop();
+          timer.stop();
+          sess.save();
+          inProgress = false;
+          System.out.print(sess);
+          sess = new Session();
+        }
+      }
+    });
+
+    this.add(sessionNumber);
     this.add(startButton);
     this.add(stopButton);
+    this.add(stopWatch);
   }
-
-  public void actionPerformed(ActionEvent e) {
-    /*
-      this.getComponent() is ugly and not stable if I add
-      more components to my UI
-    */
-    if(e.getSource() == this.getComponent(0) && !inProgress) {
-      sess.start();
-      System.out.print("Session started... ");
-      this.inProgress = true;
-    }
-    else if (e.getSource() == this.getComponent(1) && inProgress) {
-      System.out.println("Session stopped");
-      sess.stop();
-      sess.save();
-      this.inProgress = false;
-      System.out.print(sess);
-      this.sess = new Session();
-    }
-  }
-
 }
